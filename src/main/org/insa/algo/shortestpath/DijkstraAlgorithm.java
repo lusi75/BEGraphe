4e4e5314646d7bf6c1b72d17ee112cc6c65edb29
lusi75 <raf_toss_75@hotmail.fr>
2019-05-06 08:18:09 +0200

    label, dijkstra, binary heap, launch

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,4 +1,8 @@
 package org.insa.algo.shortestpath;
+import org.insa.algo.utils.*;
+import org.insa.graph.*;
+import java.awt.Label;
+import java.util.*;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
@@ -10,7 +14,14 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
+        List<Node> noeud = data.getGraph().getNodes();
+        List<Soluce> solution = 0;
+        
         // TODO:
+        if(this.ShortestPathSolution){
+            solution.append(noeud);
+            this.noeud = this.ShortestPathSolution;
+        }
         return solution;
     }
 
---
+++ src/main/org/insa/graph/Label.java
@@ -0,0 +1,50 @@
+package org.insa.graph;
+
+public class Label{
+    private Node sommet_courant;
+
+    private boolean marque;
+
+    private double count;
+
+    private Arc arc_pere;
+
+    public Label(Node sommet_courant, boolean marque, double cout, Arc arc_pere){
+        this.sommet_courant = sommet_courant;
+        this.marque = marque;
+        this.cout = cout;
+        this.arc_pere = arc_pere;
+    }
+
+    public double getSommet_courant(){
+        return this.sommet_courant;
+    }
+
+    public double setSommet_courant(Node sommet_courant){
+        this.sommet_courant = sommet_courant;
+    }
+
+    public double getCout(){
+        return cout;
+    }
+
+    public double setCout(double cout){
+        return this.cout = cout;
+    }
+
+    public double getArc_pere(){
+        return arc_pere;
+    }
+
+    public double setArc_pere(Arc arc_pere){
+        this.arc_pere = arc_pere;
+    }
+
+    public boolean isMarque(){
+        return marque;
+    }
+
+    public void setMarque(boolean marque){
+        this.marque = marque;
+    }
+}
